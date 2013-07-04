769718057844168b395b69586f65d2dbafcf04bd
Steve Hamblett <steve.hamblett@linux.com>
2013-07-04 15:09:22 +0100

    Complete, first cut

--- lib/src/CmisSession.dart
+++ lib/src/CmisSession.dart
@@ -385,8 +385,95 @@ class CmisSession{
   /**
    * Queries
    */
-   
+   void doQuery(String queryString) {
+     
+     jsonobject.JsonObject data = new jsonobject.JsonObject();
+     data.q = queryString;
+     data.cmisSelector = 'query';
+     data.maxItems = _opCtx.maxItems;
+     data.searchAllVersions = _opCtx.searchAllVersions;
+     data.includeAllowableActions = _opCtx.includeAllowableActions;           
+     data.includeRelationships = _opCtx.includeRelationships;        
+     data.skipCount = _opCtx.skipCount;
+     data.suppressResponseCodes = true; 
+     
+     String dataString = data.toString();
+     _httpRequest('POST',
+                   null,
+                   data:dataString); 
+     
+   }
   
+   void doQueryPaged(String queryString, 
+                     String elementId) {
+     
+     /* Add any found paging contexts */
+     var savedCompleter = _httpAdapter.completion;
+     
+     void localCompleter() {
+       
+       if ( _httpAdapter.jsonResponse.error == false ) {
+         
+        if ( _httpAdapter.jsonResponse.error == false ) {
+         
+           CmisPagingContext pgCtx = getPagingContext(elementId);
+           if ( pgCtx == null ) {
+            
+            addPagingContext(elementId, 
+                             0, 
+                             _httpAdapter.jsonResponse.jsonCmisResponse.numItems);
+           
+          } else {
+           
+           
+           _pagingContext[elementId].totalItems = 
+               _httpAdapter.jsonResponse.jsonCmisResponse.numItems;
+            
+          }
+           
+        }
+            
+       }
+       
+       savedCompleter();
+     } 
+     
+     jsonobject.JsonObject data = new jsonobject.JsonObject();
+     data.q = queryString;
+     data.cmisSelector = 'query';
+     data.maxItems = _opCtx.maxItems;
+     data.searchAllVersions = _opCtx.searchAllVersions;
+     data.includeAllowableActions = _opCtx.includeAllowableActions;           
+     data.includeRelationships = _opCtx.includeRelationships;        
+     data.skipCount = getPagingContextOffset(elementId);;
+     data.suppressResponseCodes = true; 
+     
+     String dataString = data.toString();
+     _httpAdapter.completion = localCompleter;
+     _httpRequest('POST',
+         null,
+         data:dataString);
+     
+   }
+   
+   /**
+    * Transactions
+    */
+   void getObjectFromTransaction(String transId) {
+     
+     jsonobject.JsonObject data = new jsonobject.JsonObject();
+     data.token = transId;
+     data.cmisSelector = 'lastResult';
+     data.suppressResponseCodes = true; 
+     
+     String dataString = data.toString();
+     _httpRequest('GET',
+         null,
+         data:dataString);
+     
+     
+   }
+   
    
   /* Updates the login credentials in Cmis that will be used for all further
    * requests to the CMIS server. Both user name and password must be set, even if one
