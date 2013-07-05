3838faba3b68df00b09a826b681ea31c9f7550fc
Steve Hamblett <steve.hamblett@linux.com>
2013-07-05 12:01:59 +0100

    Partial commit, initial interactive test html, Cmis factory started

--- test/cmis_test.dart
+++ test/cmis_test.dart
@@ -1,5 +1,38 @@
+/*
+ * Packge : Cmis
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 05/07/2013
+ * Copyright :  S.Hamblett@OSCF
+ *
+ * The Cmis class provides core functionality for interacting with CMIS servers from
+ * the browser.
+ * 
+ * This isn the CMIS client interactive test suite
+ * 
+ */
+
+import 'dart:async';
+import 'dart:json' as json;
+import 'dart:html';
+
+import '../lib/cmis.dart';
+import 'package:json_object/json_object.dart' as jsonobject;
+
 main() {
   
-  print ("Where does this come out?");
+  /* Initialise */
+  Cmis cmisClient = null;
+  
+  /* Get our working element set and add event handlers */
+  ButtonElement connectBtn = query('#cmis-connect-btn');
+  connectBtn.onClick.listen(doConnect);
+  
+}
+
+/* Event handlers */
+
+/* Connect */
+void doConnect(Event e){
+  
   
 }
